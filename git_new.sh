65d2c36c2712fb36bca28ddcc46cae0754e5d57c
SagarAI <sagarmalhotra0594@gmail.com>
2018-07-10 17:02:03 +0200

    Minor Changes

---
+++ git_new.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+startdate=2018-07-10
+enddate=2018-07-20
+
+sDateTs=`date -j -f "%Y-%m-%d" $startdate "+%s"`
+eDateTs=`date -j -f "%Y-%m-%d" $enddate "+%s"`
+dateTs=$sDateTs
+offset=86400
+
+while [ "$dateTs" -le "$eDateTs" ]
+do
+  date=`date -j -f "%s" $dateTs "+%Y-%m-%d"`
+  rand=$(((RANDOM % 5) + 1))
+        for j in $(seq 1 $rand)
+        do
+            git add .
+            git commit --date="$date" -m "Minor Changes" --allow-empty 
+            printf '%s\n' $date
+            dateTs=$(($dateTs+$offset))
+        done
+done
+
+git push -u origin master
